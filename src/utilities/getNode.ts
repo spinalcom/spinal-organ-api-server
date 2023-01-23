d0ef990b1ed6bee72325163b4e780c6d770fcc90
Laurent HA TAM DAN <l.hatamdan@spinalcom.com>
2023-01-23 12:50:36 +0100

    add dynamicId check type

--- src/utilities/getNode.ts
+++ src/utilities/getNode.ts
@@ -1,19 +1,19 @@
 /*
- * Copyright 2022 SpinalCom - www.spinalcom.com
- * 
+ * Copyright 2023 SpinalCom - www.spinalcom.com
+ *
  * This file is part of SpinalCore.
- * 
+ *
  * Please read all of the following terms and conditions
  * of the Free Software license Agreement ("Agreement")
  * carefully.
- * 
+ *
  * This Agreement is a legally binding contract between
  * the Licensee (as defined below) and SpinalCom that
  * sets forth the terms and conditions that govern your
  * use of the Program. By installing and/or using the
  * Program, you agree to abide by all the terms and
  * conditions stated or referenced herein.
- * 
+ *
  * If you do not agree to abide by these terms and
  * conditions, do not demonstrate your acceptance and do
  * not install or use the Program.
@@ -21,40 +21,44 @@
  * with this file. If not, see
  * <http://resources.spinalcom.com/licenses.pdf>.
  */
+
 import { SpinalGraphService } from 'spinal-env-viewer-graph-service';
 import type { SpinalNode } from 'spinal-model-graph';
 import type SpinalAPIMiddleware from 'src/spinalAPIMiddleware';
+import { isValidInteger } from './isValidInteger';
 
-async function getNode(spinalAPIMiddleware: SpinalAPIMiddleware, dynamicId: string, staticId: string): Promise<SpinalNode | undefined> {
-  if (dynamicId) {
+async function getNode(
+  spinalAPIMiddleware: SpinalAPIMiddleware,
+  dynamicId: string,
+  staticId: string
+): Promise<SpinalNode | undefined> {
+  if (dynamicId && isValidInteger(dynamicId)) {
     try {
       var node: SpinalNode<any> = await spinalAPIMiddleware.load(
-        parseInt(dynamicId, 10)
+        parseInt(dynamicId)
       );
-      return node
-    } catch (error) {
-    }
+      return node;
+    } catch (error) {}
   }
 
-  if (staticId && typeof staticId === "string") {
+  if (staticId && typeof staticId === 'string') {
     const node = SpinalGraphService.getRealNode(staticId);
     if (node !== undefined) {
-      return node
+      return node;
     } else {
-      const context = SpinalGraphService.getContext("spatial");
+      const context = SpinalGraphService.getContext('spatial');
       if (context === undefined) {
-        return undefined
+        return undefined;
       } else {
         const it = context.visitChildrenInContext(context);
         for await (const node of it) {
           if (node.info.id.get() === staticId) {
-            return node
+            return node;
           }
         }
       }
     }
-
   }
 }
 
-export default getNode
+export default getNode;
