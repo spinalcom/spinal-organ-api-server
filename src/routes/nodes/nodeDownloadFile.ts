89c5de6cfb50b3d935098dc7879747efaf401f67
SSRayane <r.serir@spinalcom.com>
2023-12-08 11:52:20 +0100

    Refactor file download endpoint to support Base64 encoding

--- src/routes/nodes/nodeDownloadFile.ts
+++ src/routes/nodes/nodeDownloadFile.ts
@@ -49,25 +49,32 @@ module.exports = function (
    * /api/v1/node/{id}/download_file:
    *   post:
    *     security:
-   *       - OauthSecurity:
+   *       - bearerAuth:
    *         - read
-   *     description: Download a Doc
-   *     summary: Download a Doc
+   *     description: Download a document in either binary or Base64 encoding
+   *     summary: Download a document
    *     tags:
    *       - Nodes
    *     parameters:
    *       - in: path
    *         name: id
-   *         description: use the dynamic ID
+   *         description: Use the dynamic ID
    *         required: true
    *         schema:
    *           type: integer
    *           format: int64
+   *       - in: query
+   *         name: encoding
+   *         description: Specify the encoding type for the downloaded file ('binary' or 'base64'). Default is 'binary'.
+   *         required: false
+   *         schema:
+   *           type: string
+   *           enum: [binary, base64]
    *     responses:
    *       200:
-   *         description: Download Successfully
+   *         description: File downloaded successfully
    *       400:
-   *         description: Download not Successfully
+   *         description: Error in downloading file
    */
 
   app.use('/api/v1/node/:id/download_file', async (req, res, next) => {
@@ -76,7 +83,8 @@ module.exports = function (
       var path = await spinalAPIMiddleware.load<spinal.File>(
         parseInt(req.params.id, 10)
       );
-      await down(path, res);
+      const encoding = req.query.encoding || 'binary';
+      await down(path, res, encoding);
     } catch (error) {
       console.log(error);
       res.status(400).send('ko');
@@ -84,7 +92,7 @@ module.exports = function (
   });
 };
 
-function down(file: spinal.File, res): Promise<void> {
+function down(file: spinal.File, res, encoding): Promise<void> {
   return new Promise((resolve, reject) => {
     file.load((argPath) => {
       // const p = `${__dirname}/${path.name.get()}`;
@@ -94,11 +102,27 @@ function down(file: spinal.File, res): Promise<void> {
         function (response) {
           var type =
             mime.lookup(file?.name?.get()) || 'application/octet-stream';
-          res.set('Content-Type', type);
-          response.pipe(res);
-          response.on('end', async () => {
-            resolve();
-          });
+          if (encoding === 'base64') {
+            // Change response type for base64
+            let chunks = [];
+            response.on('data', (chunk) => {
+              chunks.push(chunk);
+            });
+            response.on('end', () => {
+              let binary = Buffer.concat(chunks);
+              let base64 = binary.toString('base64');
+              res.set('Content-Type', 'text/plain');
+              res.send(base64);
+              resolve();
+            });
+          } else {
+            // Handle binary response
+            res.set('Content-Type', type);
+            response.pipe(res);
+            response.on('end', () => {
+              resolve();
+            });
+          }
           response.on('error', function (err) {
             console.log(err);
           });
